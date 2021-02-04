2fc160410b2be355382c78f8ef5a3114fcc4d06e
bignardi <vinicius.bignardi@outlook.com>
2021-02-03 22:10:02 -0300

    User selection with DELETE

--- src/main/java/com/vbt/apispringboot/resources/UserResource.java
+++ src/main/java/com/vbt/apispringboot/resources/UserResource.java
@@ -47,4 +47,11 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	// Delete by Id
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build(); // Code 204
+	}
 }
