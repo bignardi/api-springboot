28c2eebb50b4def23c246493d17aba4b3d4bca9d
bignardi <vinicius.bignardi@outlook.com>
2021-02-08 20:41:25 -0300

    Endpoint to return a user's posts

--- src/main/java/com/vbt/apispringboot/resources/UserResource.java
+++ src/main/java/com/vbt/apispringboot/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vbt.apispringboot.domain.Post;
 import com.vbt.apispringboot.domain.User;
 import com.vbt.apispringboot.dto.UserDTO;
 import com.vbt.apispringboot.services.UserService;
@@ -62,5 +63,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	// Endpoint for Posts
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
