caf25d03586ca70f9e08601394499368e42b89f2
bignardi <vinicius.bignardi@outlook.com>
2021-02-09 20:45:04 -0300

    Query with multiple criteria

--- src/main/java/com/vbt/apispringboot/services/PostService.java
+++ src/main/java/com/vbt/apispringboot/services/PostService.java
@@ -1,5 +1,6 @@
 package com.vbt.apispringboot.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	// Fetching the post until 24:00 the next day
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
